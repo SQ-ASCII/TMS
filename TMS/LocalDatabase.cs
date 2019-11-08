5eeab0c8b474ecf04622f190a6ea7e15caa87a55
Hyungbum Kim <coolish46@gmail.com>
2019-11-08 11:41:12 -0500

    LocalDatabase class initial

---
+++ TMS/LocalDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS
+{
+    /* -------------------------- LocalDatabase -------------------------- */
+    ///
+    /// \brief <i>LocalDatabase Class</i>
+    /// \details <b>Details</b>
+    /// class description here
+    class LocalDatabase
+    {
+        private int LocalDbID;
+        private string DbDirectory;
+
+        bool SetBackupDirectory(string myBackupDir)
+        {
+            bool result = false;
+            this.DbDirectory = myBackupDir;
+            return result;
+        }
+    }
+}
