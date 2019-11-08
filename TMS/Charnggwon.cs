4b4dac07a488b7985364e4a255293025d9c60c05
Charng Gwon Lee <clee9809@conestogac.on.ca>
2019-11-08 11:24:41 -0500

    add skeleton code

--- TMS/Charnggwon.cs
+++ TMS/Charnggwon.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace TMS
 {
-    class Charnggwon
-    {
-         int i;
+    class Order
+    {    
+         private int OrderID;
+         private double Capacity;
+         private int Trip;
+         private bool Complete;
+         private string DestStreetAddress;
+         private string DestCity;
+         private string DestPostalCode;
+         private string invoiceDate;
 
+         public void getOrder(int i)
+         {
+         }
 
+         public void setNewOrder()
+         {
+         }
+
+         public void setTrip()
+         {
+         }
+
+         public void completeOrder(bool b)
+         {
+         }
+
+         public void getActivityOrder()
+         {
+         }
+ 
     }
 }
