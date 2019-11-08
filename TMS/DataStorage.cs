8074d6c876bd075c8eea6bbe1c43e389f1c80486
Younchul Cho <Ycho1343@conestogac.on.ca>
2019-11-08 11:08:29 -0500

    Create default function

--- TMS/DataStorage.cs
+++ TMS/DataStorage.cs
@@ -10,7 +10,61 @@ namespace TMS
     class DataStorage
     {
         private int StorageID { get; set; }
+        private string StorageName { get; set; }
 
+        public int GetStorageId()
+        {
+            return StorageID;
+        }
 
+        public string GetStorageName()
+        {
+            return StorageName;
+        }
+
+        public void GetStorageId(int id, string name)
+        {
+            StorageID = id;
+            StorageName = name;
+        }
+
+        public void InitiateBackup()
+        {
+
+        }
+
+        public void StartBackup()
+        {
+
+        }
+
+        public void SetBillingInfo(object o)
+        {
+
+        }
+
+        public string GetContractMaket()
+        {
+            string marketName = "";
+
+            return marketName;
+        }
+
+        public void SetContractMaket(string name)
+        {
+
+        }
+
+        public void SetCarrierSystem(string carrierName)
+        {
+
+        }
+
+        public string GetCarrierSystem()
+        {
+            string carrierName = "";
+
+            return carrierName;
+        }
     }
 }
