7fbcf6e747069d5911e42203fb20c549e73a00d3
Hyungbum Kim <coolish46@gmail.com>
2019-11-08 11:32:58 -0500

    RemoteDatabase class initial

---
+++ TMS/RemoteDatabase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS
+{
+    /* -------------------------- RemoteDatabase -------------------------- */
+    ///
+    /// \brief <i>RemoteDatabase Class</i>
+    /// \details <b>Details</b>
+    /// class description here
+    class RemoteDatabase
+    {
+        private int ConnectionID;
+        private string IpAddress;
+        private string PortAddress;
+
+        public string GetIpAddress()
+        {
+            string myAddress = null;
+            return myAddress;
+        }
+
+        public string GetIpPort()
+        {
+            string myIpPort = null;
+            return myIpPort;
+        }
+
+        public void SetIpAddress(string myIpAddress)
+        {
+            this.IpAddress = myIpAddress;
+        }
+
+        public void SetIpPort(string myPort)
+        {
+            this.PortAddress = myPort;
+        }
+    }
+}
