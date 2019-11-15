7b350f2a1bf63dbcf3137dbade7e2eb50e5ee916
Hyungbum Kim <coolish46@gmail.com>
2019-11-15 11:39:29 -0500

    Logger Class Initial version

---
+++ TMS/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace TMS
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            EventLog serviceEventLog = new EventLog();
+            if (!EventLog.SourceExists("MyEventSource"))
+            {
+                EventLog.CreateEventSource("MyEventSource", "MyEventLog");
+            }
+            serviceEventLog.Source = "MyEventSource";
+            serviceEventLog.Log = "MyEventLog";
+            serviceEventLog.WriteEntry(message);
+        }
+    }
+}
