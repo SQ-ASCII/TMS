f314b8db20bd16b67c10a0ef6547baf349e48200
Younchul Cho <Ycho1343@conestogac.on.ca>
2019-11-15 22:46:24 -0500

    LogFile Class is created. It will be create and write the message into the "ServerLog.txt" file

--- TMS/Younchul.cs
+++ TMS/Younchul.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,36 @@ namespace TMS
 {
     class Younchul
     {
+    }
+
+    
+
+    public class LogFile
+    {
+        private FileStream fs = null;
+        public LogFile()
+        {
+            fs = new FileStream("ServerLog.txt", FileMode.Append);
+        }
+
+
+        public void LogClose()
+        {
+            fs.Close();
+        }
+
+
+        public void LogWrite(string msg)
+        {
+            string log = null;
+            string timeStamp = null;
+
+            timeStamp = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss" + " => ");
+            log = timeStamp + msg + "\r\n";
+
+            Console.WriteLine(log);
+            byte[] sendBuffer = Encoding.ASCII.GetBytes(log); ;
+            fs.Write(sendBuffer, 0, sendBuffer.Length);
+        }
     }
 }
