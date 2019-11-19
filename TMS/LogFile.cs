6811ce1093db45807ab53c86d3911351e43a825a
Hyungbum Kim <coolish46@gmail.com>
2019-11-19 15:19:25 -0500

    Commenting in LocalDatabase, LogFile, and RemoteDatabase Class

--- TMS/LogFile.cs
+++ TMS/LogFile.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* -- this is a LogFile COMMENT --
+	NAME	:	RemoteDatabase.cs
+	PURPOSE :	This file is to access and retrieve the information from the Remote database.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,18 +12,50 @@ using System.Threading.Tasks;
 
 namespace TMS
 {
-    /* -------------------------- LogFile -------------------------- */
+    /// 
+    /// \class RemoteDatabase
     ///
-    /// \brief <i>LogFile Class</i>
-    /// \details <b>Details</b>
-    /// class description here
+    /// \brief The purpose of this class is to model the access and get the information from the Remote database.
+    /// This class has three data member to store the Remote Database information(ConnectionID, IP,Port).
+    ///
+    /// Within the class definition :
+    /// - class data members (attributes)
+    ///	string LogID            : It will contain the LogID of LogFile
+    ///	DateTime GenerateDate   : It will contain the Generating Date of LogFile
+    ///	string UserID           : It will contain the UserID of LogFile
+    /// string ExceptionDescrip : It will contain the LogFile about Exception
+    /// 
+    /// - methods
+    ///	LogFile(void)  : default constructor
+    ///	~LogFile(void) : destructor
+    ///
+    ///	object GenLogFile()	        : method generating the Log File and returning File Stream 
+    ///	object UpdateLogFile()		: method Updating the Log File and returning File Stream 
+    ///
+    /// \author <i>Hyungbum Kim</i>
+    ///
     class LogFile
     {
+        ///
+        /// <b>Properties</b>
+        /// 
         private string LogID;
         private DateTime GenerateDate;
         private string UserID;
         private string ExceptionDescript;
 
+        ///
+        /// <b>Method GenLogFile()</b>
+        /// \brief <i>GenLogFile()</i> This method generate the Log File and returning File Stream 
+        /// \details <b>Details</b>
+        /// The purpose of this Method is generating the Log File using the data member that belongs to LogFile class and returning File Stream
+        /// 
+        /// \param myLogID   - <b>string</b>   - representation of the new LogID of LogFile
+        /// \param myGenDate - <b>DateTime</b> - representation of the Generating Date of LogFile
+        /// \param myUserID  - <b>string</b>   - representation of the UserID of LogFile
+        /// \param myUserID  - <b>string</b>   - representation of the LogFile about Exception
+        /// \return Returns the FileStream type for the new LogFile
+        /// 
         public object GenLogFile(string myLogID,DateTime myGenDate,
             string myUserID, string myException)
         {
@@ -26,6 +63,19 @@ namespace TMS
 
             return myFile;
         }
+
+        ///
+        /// <b>Method UpdateLogFile()</b>
+        /// \brief <i>UpdateLogFile()</i> This method Update the Log File and returning File Stream 
+        /// \details <b>Details</b>
+        /// The purpose of this Method is updating the Log File using the data member that belongs to LogFile class and returning File Stream
+        /// 
+        /// \param myLogID   - <b>string</b>   - representation of the update LogID of LogFile
+        /// \param myGenDate - <b>DateTime</b> - representation of the Generating Date of LogFile
+        /// \param myUserID  - <b>string</b>   - representation of the UserID of LogFile
+        /// \param myUserID  - <b>string</b>   - representation of the LogFile about Exception
+        /// \return Returns the FileStream type for the updated LogFile
+        /// 
 
         public object UpdateLogFile(string myLogID, DateTime myGenDate,
             string myUserID, string myException)
--- TMS/RemoteDatabase.cs
+++ TMS/RemoteDatabase.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* -- this is a FILEHEADER COMMENT --
+	NAME	:	RemoteDatabase.cs
+	PURPOSE :	This file is to access and retrieve the information from the Remote database.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,34 +11,94 @@ using System.Threading.Tasks;
 
 namespace TMS
 {
-    /* -------------------------- RemoteDatabase -------------------------- */
+    /// 
+    /// \class RemoteDatabase
     ///
-    /// \brief <i>RemoteDatabase Class</i>
-    /// \details <b>Details</b>
-    /// class description here
+    /// \brief The purpose of this class is to model the access and get the information from the Remote database.
+    /// This class has three data member to store the Remote Database information(ConnectionID, IP,Port).
+    ///
+    /// Within the class definition :
+    /// - class data members (attributes)
+    ///	int ConnectionID   : It will contain the ConnectionID of Remote Database
+    ///	string IpAddress   : It will contain the IpAddress of Remote Database
+    ///	string PortAddress : It will contain the PortAddress of Remote Database
+    ///
+    /// - methods
+    ///	RemoteDatabase(void) : default constructor
+    ///	~RemoteDatabase(void) : destructor
+    ///
+    ///	string GetIpAddress()				        : accessor to get a "IpAddress" data member
+    ///	string GetIpPort()				            : accessor to get a "PortAddress" data member
+    ///		  
+    ///	void SetIpAddress(string myIpAddress)	    : mutator to set a "IpAddress" data member
+    ///	void SetIpPort(string myPort)	            : mutator to set a "PortAddress" data member
+    ///
+    /// \author <i>Hyungbum Kim</i>
+    ///
     class RemoteDatabase
     {
+        ///
+        /// <b>Properties</b>
+        /// 
         private int ConnectionID;
         private string IpAddress;
         private string PortAddress;
 
+        ///
+        /// <b>Method GetIpAddress()</b>
+        /// \brief <i>GetIpAddress()</i> This method get the Remote Database IP Address and returning
+        /// \details <b>Details</b>
+        /// The purpose of this function is returning the Remote Database IP Address from the data member that belongs to RemoteDatabase class
+        /// 
+        /// \param N/A
+        /// \return Returns the string type for the Remote Database IP Address
+        /// 
         public string GetIpAddress()
         {
             string myAddress = null;
+            myAddress = this.IpAddress;
             return myAddress;
         }
 
+        ///
+        /// <b>Method GetIpPort()</b>
+        /// \brief <i>GetIpPort()</i> This method get the Remote Database Port Number and returning
+        /// \details <b>Details</b>
+        /// The purpose of this function is returning the Remote Database Port Number from the data member that belongs to RemoteDatabase class
+        /// 
+        /// \param N/A
+        /// \return Returns the string type for the Remote Database Port Number
+        /// 
         public string GetIpPort()
         {
             string myIpPort = null;
+            myIpPort = this.PortAddress;
             return myIpPort;
         }
 
+        ///
+        /// <b>Method SetIpAddress()</b>
+        /// \brief <i>SetIpAddress()</i> This method set the Remote Database IP Address
+        /// \details <b>Details</b>
+        /// The purpose of this Method is to set the Remote Database IP Address Number of Remote Database to the data member that belongs to RemoteDatabase class
+        /// 
+        /// \param myIpAddress  - <b>string</b> - representation of the new IP Address of Remote Database
+        /// \return N/A - no return value
+        /// 
         public void SetIpAddress(string myIpAddress)
         {
             this.IpAddress = myIpAddress;
         }
 
+        ///
+        /// <b>Method SetIpPort()</b>
+        /// \brief <i>SetIpPort()</i> This method set the Remote Database Port Number
+        /// \details <b>Details</b>
+        /// The purpose of this Method is to set the Remote Database Port Number of Remote Database to the data member that belongs to RemoteDatabase class
+        /// 
+        /// \param myPort  - <b>string</b> - representation of the new Port number of Remote Database
+        /// \return N/A - no return value
+        /// 
         public void SetIpPort(string myPort)
         {
             this.PortAddress = myPort;
