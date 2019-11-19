fe704f61eca59714ce77002e503a98ecb427bf66
Younchul Cho <Ycho1343@conestogac.on.ca>
2019-11-18 23:13:49 -0500

    Update the Doxyzon comment

--- TMS/DataStorage.cs
+++ TMS/DataStorage.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* -- this is a FILEHEADER COMMENT --
+	NAME	:	DataStorage.cs
+	PURPOSE :	This file is to access and retrieve the information from the database.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,65 +11,202 @@ using System.Threading.Tasks;
 
 namespace TMS
 {
-    
-    class DataStorage
+    /// 
+    /// \class DataStorage
+    ///
+    /// \brief The purpose of this class is to model the access and get the information from the data storage.
+    /// This class has two data member to store the storage information(ID, Name).
+    /// Also, it will set up the backup system to helping the administrator for how to backup.
+    ///
+    /// Within the class definition :
+    /// - class data members (attributes)
+    ///	int StorageID : It will contain the radius size of circle
+    ///	string StorageName : It will contain the side length size of square
+    ///
+    /// - methods
+    ///	DataStorage(void) : default constructor
+    ///	~DataStorage(void) : destructor
+    ///
+    ///	int GetStorageId(void)					    : accessor to get a "StorageID" data member
+    ///	string GetStorageName(void)				    : accessor to get a "StorageName" data member
+    ///		  
+    ///	void SetStorage(int id, string name)	    : mutator to set a Storage(ID and Name) data member
+    ///	void SetStorageId(int id)		            : mutator to set a "StorageID" data member
+    ///	void SetStorageName(string name)		    : mutator to set a "StorageName" data member
+    ///	
+    /// void SetBackup()                            : mutator to set up the backup system
+    /// void StartBackup()                          : mutator to start the backup system
+    /// void SetBillingInfo(object o)               : mutator to set up billing information
+    /// 
+    /// string GetCarrier(int id)                   : method to get a carrier information from the external database
+    /// string GetContractMarket(int id)            : method to get a contract market information from the external database
+    ///
+    ///
+    /// \author <i>Younchul Cho</i>
+    ///
+    public class DataStorage
     {
+        ///
+        /// <b>Properties</b>
+        /// 
         private int StorageID { get; set; }
         private string StorageName { get; set; }
 
+
+        ///
+        /// <b>Method GetStorageId()</b>
+        /// \brief <i>GetStorageId()</i> This method get the storage id and returning
+        /// \details <b>Details</b>
+        /// The purpose of this function is returning the ID of storage from the data member that belongs to DataStorage class
+        /// 
+        /// \param N/A
+        /// \return Returns the integer type for the ID of a storage
+        /// 
         public int GetStorageId()
         {
             return StorageID;
         }
 
+
+        ///
+        /// <b>Method GetStorageName()</b>
+        /// \brief <i>GetStorageName()</i> This method get the storage name and returning
+        /// \details <b>Details</b>
+        /// The purpose of this function is returning the name of storage from the data member that belongs to DataStorage class
+        /// 
+        /// \param N/A
+        /// \return Returns the string type for the name of a storage
+        /// 
         public string GetStorageName()
         {
             return StorageName;
         }
 
-        public void GetStorageId(int id, string name)
+
+        ///
+        /// <b>Method SetStorage()</b>
+        /// \brief <i>SetStorage()</i> This method set the storage id and name
+        /// \details <b>Details</b>
+        /// The purpose of this function is set the name and ID of storage to the data member that belongs to DataStorage class
+        /// 
+        /// \param id  - <b>int</b> - representation of the new id number of storage
+        /// \param name  - <b>string</b> - representation of the new name of storage
+        /// \return N/A - no return value
+        /// 
+        public void SetStorage(int id, string name)
+        {
+            SetStorageId(id);
+            SetStorageName(name);
+        }
+
+
+        ///
+        /// <b>Method SetStorageId()</b>
+        /// \brief <i>SetStorageId()</i> This method set the storage id
+        /// \details <b>Details</b>
+        /// The purpose of this function is set the ID of storage to the data member that belongs to DataStorage class
+        /// 
+        /// \param id  - <b>int</b> - representation of the new id number of storage
+        /// \return N/A - no return value
+        /// 
+        public void SetStorageId(int id)
         {
             StorageID = id;
+        }
+
+
+        ///
+        /// <b>Method SetStorageName()</b>
+        /// \brief <i>SetStorageName()</i> This method set the storage name
+        /// \details <b>Details</b>
+        /// The purpose of this function is set the name of storage to the data member that belongs to DataStorage class
+        /// 
+        /// \param name  - <b>string</b> - representation of the new name of storage
+        /// \return N/A - no return value
+        /// 
+        public void SetStorageName(string name)
+        {
             StorageName = name;
         }
 
-        public void InitiateBackup()
+
+        ///
+        /// <b>Method SetBackup()</b>
+        /// \brief <i>SetBackup()</i> This method set up the backup system for running
+        /// \details <b>Details</b>
+        /// The purpose of this function is set up the backup system to store every information to the external database
+        /// 
+        /// \param N/A
+        /// \return N/A - no return value
+        /// 
+        public void SetBackup()
         {
 
         }
 
+
+        ///
+        /// <b>Method StartBackup()</b>
+        /// \brief <i>StartBackup()</i> This method initiates the backup system
+        /// \details <b>Details</b>
+        /// The purpose of this function is to start the backup into the external database or file
+        /// 
+        /// \param N/A
+        /// \return N/A - no return value
+        /// 
         public void StartBackup()
         {
 
         }
 
+
+        ///
+        /// <b>Method SetBillingInfo()</b>
+        /// \brief <i>SetBillingInfo()</i> This method set up the billing information
+        /// \details <b>Details</b>
+        /// The purpose of this function is to set the information into the billing information
+        /// 
+        /// \param o  - <b>object</b> - The object which triggered with thread as a worker
+        /// \return N/A - no return value
+        /// 
         public void SetBillingInfo(object o)
         {
 
         }
 
-        public string GetContractMaket()
-        {
-            string marketName = "";
 
-            return marketName;
+        ///
+        /// <b>Method GetContractMarket()</b>
+        /// \brief <i>GetContractMarket()</i> This method get the contract market(customer) name
+        /// \details <b>Details</b>
+        /// The purpose of this function is get the contract market information from the external database
+        /// 
+        /// \param id  - <b>int</b> - representation of id that wants to know for contract market
+        /// \return Returns the string type of name which indicate the id of the contract market
+        /// 
+        public string GetContractMarket(int id)
+        {
+            string name = "";
+
+            return name;
         }
 
-        public void SetContractMaket(string name)
+
+
+        ///
+        /// <b>Method GetCarrier()</b>
+        /// \brief <i>GetCarrier()</i> This method get the carrier(driver) name
+        /// \details <b>Details</b>
+        /// The purpose of this function is get the carrier information from the external database
+        /// 
+        /// \param id  - <b>int</b> - representation of id that wants to know for carrier
+        /// \return Returns the string type of name which indicate the id of the carrier
+        ///
+        public string GetCarrier(int id)
         {
+            string name = "";
 
-        }
-
-        public void SetCarrierSystem(string carrierName)
-        {
-
-        }
-
-        public string GetCarrierSystem()
-        {
-            string carrierName = "";
-
-            return carrierName;
+            return name;
         }
     }
 }
---
+++ TMS/FileLog.cs
@@ -0,0 +1,94 @@
+﻿/* -- this is a FILEHEADER COMMENT --
+	NAME	:	FileLog.cs
+	PURPOSE :	This file is to create and write the log into the file.
+*/
+
+using System;
+using System.IO;
+using System.Text;
+
+
+namespace TMS
+{
+    /// 
+    /// \class FileLog
+    ///
+    /// \brief The purpose of this class is to model the create and write the log file into the file.
+    /// This class has one data member to indicate the file stream.
+    /// Also, it will store the message with a current date
+    ///
+    /// Within the class definition :
+    /// - class data members (attributes)
+    ///	FileStream fs : It will indicate the file, which is the specific information to write the log.
+    ///
+    /// - methods
+    /// FileLog(void)                   : default constructor
+    ///	void LogClose()                 : method to close the file system
+    ///	void LogWrite(string msg)       : method to write the log message with date information in the file
+    ///	
+    /// \author <i>Younchul Cho</i>
+    ///
+    public class FileLog
+    {
+        ///
+        /// <b>Properties</b>
+        /// 
+        FileStream fs = null;
+
+
+        ///
+        /// <b>Method FileLog()</b>
+        /// \brief <i>FileLog()</i> To instantiate a new FileLog object
+        /// \details <b>Details</b>
+        /// This is a FileLog's default constructor, it will set up the file stream.
+        /// The default file name is "LogFile.txt" with append mode.
+        /// 
+        /// \param N/A
+        /// \return As this is a <i>default constructor</i> for the FileLog class, nothing is returned
+        /// 
+        public FileLog()
+        {
+            fs = new FileStream("LogFile.txt", FileMode.Append);
+        }
+
+
+
+        ///
+        /// <b>Method LogClose()</b>
+        /// \brief <i>LogClose()</i> This method close the file system
+        /// \details <b>Details</b>
+        /// The purpose of this function is closing the file system when the system does not need to write the log.
+        /// 
+        /// \param N/A
+        /// \return N/A - no return value
+        /// 
+        public void LogClose()
+        {
+            fs.Close();
+        }
+
+
+
+        ///
+        /// <b>Method LogWrite()</b>
+        /// \brief <i>LogWrite()</i> This method writes the log message into the file
+        /// \details <b>Details</b>
+        /// The purpose of this function is writing the message with the current date into the file
+        /// 
+        /// \param msg  - <b>string</b> - representation of the message which is want to write into the file
+        /// \return N/A - no return value
+        /// 
+        public void LogWrite(string msg)
+        {
+            string log = null;
+            string timeStamp = null;
+
+            timeStamp = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss" + " => ");
+            log = timeStamp + msg + "\r\n";
+
+            Console.WriteLine(log);
+            byte[] sendBuffer = Encoding.ASCII.GetBytes(log); ;
+            fs.Write(sendBuffer, 0, sendBuffer.Length);
+        }
+    }
+}
