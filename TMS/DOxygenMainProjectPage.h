3f6908bc59aac80e4b85d06843a7548287161fca
YC-Cho <44073202+YC-Cho@users.noreply.github.com>
2019-11-21 17:05:31 -0500

    Create DOxygenMainProjectPage.h
    Doxygen main page

---
+++ TMS/DOxygenMainProjectPage.h
@@ -0,0 +1,57 @@
+/// \mainpage TMS Project   
+/// \note This application is a transportation management system for Omnicorp Shipping Handling and Transportation members
+/// \image html SimpleDog.jpg
+///
+///
+/// \section intro Program Introduction
+/// This <b>TMS</b> (Transportation Management System) is a system that allows 
+/// <i>Omnicorp Shipping Handling and Transportation</i> (OSHT) staff members to 
+/// manage the order creation, management and billing process for the company.
+///
+///
+/// Major TMS features to note are:
+/// 
+/// - This application can connect the external database to get/update the information.
+/// - The <b>administrator</b> will set up the DBMS location (remote/local).
+/// - The <b>administrator</b> will change the information.
+/// - The <b>buyer</b> may review the customer and create the new order, which is request by customers.
+/// - The <b>buyer</b> can be store the detailed billing information into the database as a text file.
+/// - The <b>planner</b> review the order information and complete that.
+///
+///
+/// That list of major TMS features are:
+/// -# This applicaiton is using <b>WPF</b> GUI applcation and using backend database(<b>MySQL</b>).
+/// -# Execute the program <i>different by user type</i>.
+/// -# The <i>information</i> is going to <i>share</i> with the staff members.
+/// -# The order will be filled up, complete, and confirmed by different members.
+///
+///
+///
+/// <hr>
+/// \section notes Special Release Notes
+/// Version:
+/// - V 1.0 : Create a skeleton of this application (<b><i>class</i></b> and <b><i>method</i></b>).
+///
+///
+///
+///
+/// <hr>
+/// \section requirements Project Requirements
+///
+/// This program is developed by <b>Visual Studio 2019</b> in the <i>window platform</i>.
+///
+/// <hr> 
+/// \todo [optionally include text about more work to be done]
+/// \todo Give each todo item its own line
+///
+/// <hr>
+/// \section version Current version of the Dog Project :
+/// <ul>
+/// <li>\authors   <b><i>ASCII</i></b> Team !!</li>
+/// <li>\version   1.00.00</li>
+/// <li>\date      2019.Oct-Dec</li>
+/// <li>\pre       This application will be running on a Windows operating system</li>
+/// <li>\warning   Your computer has to connect the internet if you want to use a remote database</li>
+/// <li>\copyright ASCII Team in SENG2020</li>
+/// </ul>
+///
